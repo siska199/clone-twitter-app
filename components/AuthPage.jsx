f3aed613c9cba60d1da8ede93304ce4b1f0b2174
siska199 <siska.apriana.1703226@students.um.ac.id>
2024-06-28 11:40:08 +0700

    update

--- components/AuthPage.jsx
+++ components/AuthPage.jsx
@@ -47,48 +47,18 @@ const AuthPage = ({ providers }) => {
           <h5 className="font-bold text-[1.7rem] mb-4 tracking-[0.15rem]">
             Join Twitter Today
           </h5>
-          <div className="flex flex-col space-y-3 text-black w-[60%] lg:w-full  text-sm ">
-            {/* <button
-              onClick={(e) => handleSignUp(e)}
-              className="bg-sky-600 rounded-3xl py-[0.5rem] font-medium text-white hover:bg-sky-500"
-            >
-              Sign Up with phone or email
-            </button>
-            <div className="text-center">
-              <p className="line relative text-white ">OR</p>
-            </div> */}
-            {Object.values(providers).map((provider) => (
+          {Object.values(providers).map((provider) => (
               <button
                 onClick={(e) => handleSignUp(e, provider.id)}
                 key={provider.name}
-                className="bg-gray-200 flex flex-col mx-auto w-fit px-4 rounded-3xl py-[0.5rem] flex justify-center items-center font-medium hover:bg-gray-300"
+                className="bg-gray-200 text-black mx-auto w-full md:w-fit px-4 rounded-3xl py-[0.5rem] flex justify-center items-center font-medium hover:bg-gray-300"
               >
                 <FcGoogle className="mr-3" />
                 Sign In with {provider.name}
               </button>
             ))}
-            {/* <p className="text-gray-400 text-[0.6rem] leading-3">
-              By signing up, you agree to the{" "}
-              <span className="text-sky-400"> Terms of Service</span> and{" "}
-              <span className="text-sky-400">Privacy Policy,</span>{" "}
-              <span className="text-sky-400">including Cookie Use</span>.
-            </p> */}
-          </div>
-          {/* <div className="flex flex-col w-[60%] lg:w-[40%] mt-[3rem]">
-            <h5 className="font-medium mb-3 text-medium">
-              Already have account
-            </h5>
-            <button
-              onClick={(e) => handleSignIn(e)}
-              className="border-[0.05rem] rounded-3xl py-[0.5rem] font-medium text-sm text-sky-500 hover:bg-gray-800"
-            >
-              Sign In
-            </button>
-          </div> */}
         </form>
       </section>
-      {/* {modalSignUp && <SignUp />}
-      {modalSignIn && <SignIn providers={providers} />} */}
     </Layout>
   );
 };
